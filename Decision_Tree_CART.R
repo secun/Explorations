f27de441a0590efcec661170ed387fef6559442f
Secun <secundino.sexto@gmail.com>
2021-06-14 16:56:25 +0200

    small change

--- Decision_Tree_CART.R
+++ Decision_Tree_CART.R
@@ -1,4 +1,4 @@
- 
+#Modification on decision tree file
 library(tidyverse) #data manipulation
 library(caret) #ML workflow
 library(rpart) #decision tree models
@@ -8,16 +8,16 @@ library(rpart) #decision tree models
 #data variables
 dat_tree<- dat_UB_by_order [,-(1:2)]
 
-#The outcome variable is continuous, so 
+#The outcome variable is continuous, so
 #instead of decision tree shoudl be a regression tree
 #split data and create train/test data
 set.seed(123)
-training.samples <- dat_tree$discount. %>% createDataPartition(p=0.8, list=FALSE) 
+training.samples <- dat_tree$discount. %>% createDataPartition(p=0.8, list=FALSE)
 train.data <- dat_tree [training.samples,]
 test.data <- dat_tree [-training.samples,]
 
 #Create model for train data
-model <- train( discount. ~., data=train.data, method ="rpart", 
+model <- train( discount. ~., data=train.data, method ="rpart",
                 trControl =trainControl("cv",number=10),
                 tuneLength=10
               )
@@ -35,8 +35,8 @@ predictions <- model %>% predict (test.data)
 head(predictions)
 RMSE(predictions,test.data$discount.)
 
-  
-  
+
+
 #Make prediction for one single data
 newdata <- data.frame(Sepal.Length=6.5, Sepal.Width=3.0,
                       Petal.Length=5.2,Petal.Width=2.0)
